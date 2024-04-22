09299583a3b314918b06b96ca4e57b5a0dc4c2a7
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-04-21 23:22:52 -0500

    procastinate on job

---
+++ dataclasses.py
@@ -0,0 +1,11 @@
+# from dataclasses import dataclass
+
+# @dataclass
+# class Comment:
+#     id:str
+#     user:str
+#     text:str
+
+
+# def main(0):
+#     Comment = Comment(1,'siddh','hello world')
---
+++ dataclasses2.py
@@ -0,0 +1,4 @@
+def dc():
+    return("hello world")
+
+dc()
