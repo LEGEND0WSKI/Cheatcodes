3bb520b07585d076d6c3ece379580d3651e78b55
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-04-26 01:55:09 -0500

    cool design

---
+++ graphicdesign.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.bgcolor("black")
+squarey = turtle.Turtle()
+squarey.speed(20)
+squarey.pencolor("red")
+for i in range(400):
+    squarey.forward(i)
+    squarey.left(91)
+    
